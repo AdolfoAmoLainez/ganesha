c197eaeb3411795968a1de4a7c2e79aca86208f9
Adolfo <adolfo.amolainez@gmail.com>
2019-07-25 15:06:47 +0200

    Scripts de sistema

---
+++ oscripts/ganesha-config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+
+HOME_GANESHA="/home/samba/assignatures"
+
+codigo_200="Todo correcto"
+
+codigo_501="El usuario no existe o no puede añadirse al grupo local"
+codigo_502="El grupo local no existe"
+codigo_503="El directorio de la asignatura no se puede crear, ya existe"
+codigo_504="No se han podido cambiar los permisos del directorio de la asignatura, motivo desconocido"
+codigo_505="El directorio de la asignatura no existe"
+codigo_506="El directorio del grupo de la asignatura no existe"
+codigo_507="El grupo local de la asignatura no existe"
+codigo_508="El grupo local del grupo de la asignatura no existe"
+codigo_509="No se puede añadir el usuario al grupo local de la asignatura, motivo desconocido"
+codigo_510="No se puede añadir el usuario al grupo local del grupo de la asignatura, motivo desconocido"
+codigo_511="El directorio de la asignatura no se puede crear, motivo desconocido"
+codigo_512="El grupo local de asignatura no se puede crear, ya existe"
+codigo_513="El profesor no puede añadirse al grupo local de asignatura, motivo desconocido"
+codigo_514="El directorio del grupo de la asignatura no se puede crear, ya existe" 
+codigo_515="El grupo local del grupo de la asignatura no se puede crear, ya existe"
+codigo_516="El profesor no puede añadirse al grupo local del grupo de la asignatura, motivo desconocido"
+codigo_517="El directorio del grupo de la asignatura no se puede crear, motivo desconocido"
+codigo_518="El grupo local del grupo de la asignatura no se puede crear, motivo desconocido"
+codigo_519="No se han podido cambiar los permisos del directorio del grupo de la asignatura, motivo desconocido"
+codigo_520="El alumno no puede borrarse del grupo local del grupo de la asignatura, motivo desconocido"
+codigo_521="El alumno no puede borrarse del grupo local de la asignatura, motivo desconocido"
+codigo_522="El directorio del grupo de la asignatura no se puede renombrar, ya existe un directorio con el mismo nombre"
+codigo_523="El grupo local del grupo de la asignatura no se puede renombrar, ya existe un grupo local con el mismo nombre"
+codigo_524="El grupo local del grupo de la asignatura no se puede renombrar, motivo desconocido"
+codigo_525="El directorio del grupo de la asignatura no se puede renombrar, motivo desconocido"
+codigo_526="La quota del grupo de la asignatura no se puede cambiar, motivo desconocido"
+codigo_527="El directorio o el grupo local del grupo de la asignatura no existe, o el nuevo directorio o grupo local del grupo de la asignatura ya exite"
+
+codigo_599="Faltan parámetros"
