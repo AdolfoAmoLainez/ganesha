0712f8b7b154ef0740f1c760242aed9a225385c3
Adolfo <adolfo.amolainez@gmail.com>
2019-09-13 10:27:45 +0200

    Canviadas variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,8 +7,15 @@
 }; */
 export const environment = {
   production: true,
+  selfApiUrl: 'https://ganesha.uab.cat/selfapi/',
+  loginApiUrl: 'https://ganesha.uab.cat/loginapi/',
+  apiCrudUrl: 'https://ganesha.uab.cat/api/crud/',
+  apiCustomUrl: 'https://ganesha.uab.cat/api/custom/'
+};
+/* export const environment = {
+  production: true,
   selfApiUrl: 'http://chaman.uab.cat:3000/selfapi/',
   loginApiUrl: 'http://chaman.uab.cat:3000/loginapi/',
   apiCrudUrl: 'http://chaman.uab.cat:3000/api/crud/',
   apiCustomUrl: 'http://chaman.uab.cat:3000/api/custom/'
-};
+}; */
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,26 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+/* export const environment = {
   production: false,
   selfApiUrl: 'http://localhost:3000/selfapi/',
   apiCrudUrl: 'http://localhost:3000/api/crud/',
   apiCustomUrl: 'http://localhost:3000/api/custom/'
+}; */
+/* export const environment = {
+  production: false,
+  selfApiUrl: 'http://minisiva.ccomunicacio:3000/selfapi/',
+  loginApiUrl: 'http://minisiva.ccomunicacio:3000/loginapi/',
+  apiCrudUrl: 'http://minisiva.ccomunicacio:3000/api/crud/',
+  apiCustomUrl: 'http://minisiva.ccomunicacio:3000/api/custom/'
+}; */
+export const environment = {
+  production: true,
+  selfApiUrl: 'https://ganesha.uab.cat/selfapi/',
+  loginApiUrl: 'https://ganesha.uab.cat/loginapi/',
+  apiCrudUrl: 'https://ganesha.uab.cat/api/crud/',
+  apiCustomUrl: 'https://ganesha.uab.cat/api/custom/'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
