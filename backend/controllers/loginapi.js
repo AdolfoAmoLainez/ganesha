74dbf7fea2ebb3f3dde27c15d750e403a2cebde2
Adolfo Amo <adolfo.amo@uab.cat>
2022-04-21 13:24:07 +0200

    Canvis al nou CAS

--- backend/controllers/loginapi.js
+++ backend/controllers/loginapi.js
@@ -2,17 +2,16 @@ const SelfApiController = require("./selfapi");
 var cas = require('connect-cas');
 var url = require('url');
 
-cas.configure({ 'host': 'sacnt.uab.cat', 'protocol': 'https',
+cas.configure({ 'host': 'sso.uab.cat', 'protocol': 'https',
 paths: {
         validate: '/validate',
-        serviceValidate: '/p3/serviceValidate', // CAS 3.0
-        proxyValidate: '/p3/proxyValidate', // CAS 3.0
+        serviceValidate: '/cas/serviceValidate', // CAS 3.0
+        proxyValidate: '/cas/proxyValidate', // CAS 3.0
         proxy: '/proxy',
-        login: '/login',
+        login: '/cas',
         logout: '/logout'
     }
 });
-
 
 /**
  * Request:
--- backend/rutes/loginapi.js
+++ backend/rutes/loginapi.js
@@ -6,13 +6,13 @@ var url = require('url');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 
-cas.configure({ 'host': 'sacnt.uab.cat', 'protocol': 'https',
+cas.configure({ 'host': 'sso.uab.cat', 'protocol': 'https',
 paths: {
         validate: '/validate',
-        serviceValidate: '/p3/serviceValidate', // CAS 3.0
-        proxyValidate: '/p3/proxyValidate', // CAS 3.0
+        serviceValidate: '/cas/serviceValidate', // CAS 3.0
+        proxyValidate: '/cas/proxyValidate', // CAS 3.0
         proxy: '/proxy',
-        login: '/login',
+        login: '/cas',
         logout: '/logout'
     }
 });
